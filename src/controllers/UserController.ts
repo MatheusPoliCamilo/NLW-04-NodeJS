f8076bea7769b3567e98fec8d2c1361de2052ef7
Matheus Poli <matheuspolicamilo@gmail.com>
2021-02-24 23:53:30 -0300

    Create Surveys and Users specs

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,32 +1,33 @@
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
   async create(request: Request, response: Response) {
-    const { name, email } = request.body
+    const { name, email } = request.body;
 
-    const usersRepository = getRepository(User)
+    const usersRepository = getCustomRepository(UsersRepository);
 
     // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
     const userAlreadyExists = await usersRepository.findOne({
-      email
-    })
+      email,
+    });
 
     if (userAlreadyExists) {
       return response.status(400).json({
-        error: "User already exists!"
-      })
+        error: "User already exists!",
+      });
     }
 
     const user = usersRepository.create({
-      name, email
-    })
+      name,
+      email,
+    });
 
-    await usersRepository.save(user)
+    await usersRepository.save(user);
 
-    return response.json(user)
+    return response.status(201).json(user);
   }
 }
 
-export { UserController }
+export { UserController };
